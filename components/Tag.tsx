8586294b708b6e067dbce1ba013f21562d5eb5b7
Ryan Larkin <rlarkin212@gmail.com>
2020-07-12 16:40:35 +0100

    lint

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,23 +1,22 @@
 import { styled } from "../utils/theme";
 
-const Tag = styled.a `
-    display: inline-block;
-    background: transparent;
-    padding: 10px;
-    background: ${(x) => x.theme.accent};
-    transition: background-color 150ms ease;
-    font-size: 18px;
-    cursor: pointer;
-    margin: 0px 15px 30px 0px;
-    &:last-child {
-        margin-right: 0;
-       }
-    border-radius: 15px;
+const Tag = styled.a`
+  display: inline-block;
+  background: transparent;
+  padding: 10px;
+  background: ${(x) => x.theme.accent};
+  transition: background-color 150ms ease;
+  font-size: 18px;
+  cursor: pointer;
+  margin: 0px 15px 30px 0px;
+  &:last-child {
+    margin-right: 0;
+  }
+  border-radius: 15px;
 
-    &:hover{
-        background: ${(x) => x.theme.accentDark};
-    }
-
+  &:hover {
+    background: ${(x) => x.theme.accentDark};
+  }
 `;
 
 export default Tag;
