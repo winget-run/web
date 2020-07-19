93aafac9e7aaf0a80b5d18ff117cc3bc8712b7d8
Ryan Larkin <rlarkin212@gmail.com>
2020-07-19 18:18:40 +0100

    added check for in search

--- components/DownloadModal.tsx
+++ components/DownloadModal.tsx
@@ -310,7 +310,7 @@ const DownloadModal = () => {
                 >
                   <CardIcon
                     src={
-                      e.Package.IconUrl || getIcon(e.Package.Latest.Homepage)
+                      e.Package.IconUrl || getIcon(e.Package.Latest.Homepage, false)
                     }
                     alt=""
                   />
