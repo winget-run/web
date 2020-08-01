f7e0b12863333608bd776792dddd9e55a2025511
Matthew Watt <MattheousDT@users.noreply.github.com>
2020-08-01 13:21:58 +0100

    Search tips :eyes:

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 
 import styled from "../utils/theme";
 import useDebounce from "../utils/hooks/useDebounce";
@@ -136,6 +136,7 @@ const ResultsContainer = styled.div`
   border-radius: 8px;
   box-shadow: 0 3px 15px rgba(0, 0, 0, 0.5);
   opacity: 0;
+  overflow: hidden;
   transform: translateY(-10px);
   pointer-events: none;
   transition: 150ms ease;
@@ -148,11 +149,9 @@ const ResultsContainer = styled.div`
   }
 `;
 
-const AllResults = styled.a`
-  float: right;
+const BottomButton = styled.a`
   color: ${(x) => x.theme.darkGrey};
   font-weight: 700;
-  margin-top: 15px;
   &:hover {
     text-decoration: underline;
   }
@@ -166,6 +165,26 @@ const NoResultsText = styled.h4`
   text-align: center;
   font-size: 24px;
   margin: 15px 0;
+`;
+
+const SearchOptions = styled.div`
+  background-color: ${(x) => x.theme.dirtyWhite};
+  margin: -20px -20px -31px;
+  padding: 20px 20px 40px;
+  color: ${(x) => x.theme.background};
+
+  p {
+    margin: 0 0 8px;
+    font-size: 16px;
+    &:last-child {
+      margin: 0;
+    }
+    code {
+      font-family: Consolas, monospace;
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
 `;
 
 interface IProps {
@@ -185,6 +204,8 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
     updateClear,
   } = useContext(SearchContext);
   const debouncedSearchTerm = useDebounce(search?.term ?? "", 400);
+  const [focus, setFocus] = useState(false);
+  const [showSearchOptions, setShowSearchOptions] = useState(false);
 
   useEffect(() => {
     if (
@@ -217,8 +238,9 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
   };
 
   const handleSearch = () => {
-    router.push({ pathname: "/search", query: search.filters as string });
     updateSearchTerm("");
+    router.push({ pathname: "/search", query: search.filters as string });
+    updateClear();
   };
 
   return (
@@ -238,6 +260,8 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
           onChange={(e) => handleUpdateSearch(e.target.value)}
           inNav={inNav}
           onKeyPress={(e) => e.key === "Enter" && handleSearch()}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
         />
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -270,13 +294,7 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
         {(!resultsHidden ?? true) && (
           <ResultsContainer
             aria-live="polite"
-            aria-modal={
-              !!(
-                debouncedSearchTerm &&
-                search?.results != null &&
-                search?.term.length > 1
-              )
-            }
+            aria-modal={!!search?.term || focus}
           >
             {search?.results != null &&
               search?.term != "" &&
@@ -296,10 +314,32 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
                       iconUrl={e.IconUrl}
                     />
                   ))}
-                  <AllResults onClick={handleSearch}>
-                    View all results
-                    <img src={require("./icons/arrow.svg")} alt="" />
-                  </AllResults>
+                  <div
+                    css={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      marginTop: "20px",
+                    }}
+                  >
+                    <BottomButton
+                      onClick={() => setShowSearchOptions(!showSearchOptions)}
+                    >
+                      Search options
+                      <img
+                        src={require("./icons/chevron-down-blue.svg")}
+                        alt=""
+                        css={
+                          showSearchOptions && {
+                            transform: "rotate(180deg) translateY(2px)",
+                          }
+                        }
+                      />
+                    </BottomButton>
+                    <BottomButton onClick={handleSearch}>
+                      View all results
+                      <img src={require("./icons/arrow.svg")} alt="" />
+                    </BottomButton>
+                  </div>
                 </>
               )}
             {search?.results != null &&
@@ -309,6 +349,33 @@ const Search = ({ inNav, hidden, resultsHidden }: IProps) => {
                   No results found for "{debouncedSearchTerm}"
                 </NoResultsText>
               )}
+            {(search?.results?.Packages?.length === 0 ||
+              search?.results === null ||
+              showSearchOptions) && (
+              <SearchOptions
+                css={
+                  search.results?.Packages?.length === 0 ||
+                  (search.results?.Packages?.length > 0 && showSearchOptions)
+                    ? { marginTop: "15px" }
+                    : {}
+                }
+              >
+                <p>
+                  <code>name:</code> Searches only the package names.
+                </p>
+                <p>
+                  <code>publisher:</code> Searches for packages by a publisher.
+                </p>
+                <p>
+                  <code>description:</code> Searches only for keywords in the
+                  description.
+                </p>
+                <p>
+                  <code>tags:</code> Searches for packages with a specific tags
+                  (comma seperated).
+                </p>
+              </SearchOptions>
+            )}
           </ResultsContainer>
         )}
       </WidthWrapper>
