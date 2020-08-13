e90ca2d320e9b7a277b03012f835f0f2ccf72d1c
Matthew Watt <MattheousDT@users.noreply.github.com>
2020-08-13 16:52:18 +0100

    I replaced this already eslint you fucking piece of shit

--- utils/helperFunctions.tsx
+++ utils/helperFunctions.tsx
@@ -28,9 +28,9 @@ export const regexWrapJSX = (
   if (!regexes.length) {
     return input;
   }
-  let split = input.split(regexes[0]);
-  let replacements = input.match(regexes[0]);
-  let result = [];
+  const split = input.split(regexes[0]);
+  const replacements = input.match(regexes[0]);
+  const result = [];
   for (let i = 0; i < split.length - 1; i++) {
     result.push(regexWrapJSX(split[i], regexes.slice(1)));
     result.push(<span>{replacements[i]}</span>);
