6648a3ef6c53862b758927f32d5a8515091a196d
Matthew Watt <MattheousDT@users.noreply.github.com>
2020-08-13 16:35:53 +0100

    how the hell did this even get there

--- utils/helperFunctions.tsx
+++ utils/helperFunctions.tsx
@@ -1,4 +1,4 @@
-constimport { ISearchFilters } from "./state/Search";
+import { ISearchFilters } from "./state/Search";
 
 export const getIcon = (url: string | null, isSearch: boolean): string => {
   if (url) {
@@ -28,9 +28,9 @@ export const regexWrapJSX = (
   if (!regexes.length) {
     return input;
   }
-  const split = input.split(regexes[0]);
-  const replacements = input.match(regexes[0]);
-  const result = [];
+  let split = input.split(regexes[0]);
+  let replacements = input.match(regexes[0]);
+  let result = [];
   for (let i = 0; i < split.length - 1; i++) {
     result.push(regexWrapJSX(split[i], regexes.slice(1)));
     result.push(<span>{replacements[i]}</span>);
